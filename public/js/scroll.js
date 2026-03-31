ef1c91630d8cf2eaee1c3c30944c3161b04cb445
Julien Millon <j.millon@orange.fr>
2026-03-31 13:57:59 +0200

    Nouveau Base html + scroll js

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const scrollTopBtn = document.createElement('button');
     scrollTopBtn.innerHTML = '&uarr;';
     scrollTopBtn.id = 'scroll-top-btn';
+    scrollTopBtn.style.display = 'none';
+    scrollTopBtn.style.position = 'fixed';
+    scrollTopBtn.style.bottom = '20px';
+    scrollTopBtn.style.right = '20px';
     document.body.appendChild(scrollTopBtn);
 
     //la le bouton apparait au bout de 150 pixel
